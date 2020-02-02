fd346a7085e8800959ed52091ccc6525b0319b6b
Kay McCormick <kay@kaymccormick.com>
2020-02-01 18:32:59 -0800

    clan up

--- WpfApp1Tests3/Attributes/AppInitializeAttribute.cs
+++ WpfApp1Tests3/Attributes/AppInitializeAttribute.cs
@@ -19,64 +19,59 @@ using Xunit.Sdk ;
 
 namespace WpfApp1Tests3.Attributes
 {
-	public class AppInitializeAttribute : BeforeAfterTestAttribute
-	{
-		private static readonly Logger Logger = null ;
+    public class AppInitializeAttribute : BeforeAfterTestAttribute
+    {
+        private static readonly Logger Logger = null ;
 
-		public App MyApp { get ; set ; }
-		// LogManager.GetCurrentClassLogger();
-
-
-		/// <summary>
-		///     This method is called after the test method is executed.
-		/// </summary>
-		/// <param name="methodUnderTest">The method under test</param>
-		public override void After ( MethodInfo methodUnderTest )
-		{
-			base.After ( methodUnderTest ) ;
-		}
-
-		/// <summary>
-		///     This method is called before the test metphod is executed.
-		/// </summary>
-		/// <param name="methodUnderTest">The method under test</param>
-		public override void Before ( MethodInfo methodUnderTest )
-		{
-			LoggerProxyHelper.LogMethod logMethod = LogMethod ;
-			// Logger.Trace ( $"Nefore" ) ;
-			// Application.LoadComponent (
-			// Application.Current
-			// , new Uri ( "Applications/App.xaml" )
-
-			// ) ;
-			MyApp = Application.Current as App ;
+        public App MyApp { get ; set ; }
+        // LogManager.GetCurrentClassLogger();
 
 
-			if ( MyApp        != null
-			     && MyApp.TCS != null )
-			{
-				if ( MyApp.TCS.Task != null )
-				{
-					logMethod ( "Waiting for task to complete" ) ;
-					MyApp.TCS.Task.Wait ( ) ;
-				}
-				else
-				{
-					logMethod ( "null" ) ;
-				}
-			}
-			else
-			{
-				logMethod ( "null" ) ;
-			}
+        /// <summary>
+        ///     This method is called before the test method is executed.
+        /// </summary>
+        /// <param name="methodUnderTest">The method under test</param>
+        public override void Before ( MethodInfo methodUnderTest )
+        {
+            LoggerProxyHelper.LogMethod logMethod = LogMethod ;
+            
+            // Application.LoadComponent (
+            // Application.Current
+            // , new Uri ( "Applications/App.xaml" )
 
-			base.Before ( methodUnderTest ) ;
-		}
+            // ) ;
+            MyApp = Application.Current as App ;
 
-		private void LogMethod ( string message , string callerfilepath , string callermembername )
-		{
-			Logger?.Debug ( message ) ;
-			Debug.WriteLine ( message ) ;
-		}
-	}
+
+            // TODO - this is horribly broken
+            if ( MyApp        != null
+                 && MyApp.TCS != null )
+            {
+                if ( MyApp.TCS.Task != null )
+                {
+                    logMethod ( "Waiting for task to complete" ) ;
+                    MyApp.TCS.Task.Wait ( ) ;
+                }
+                else
+                {
+                    logMethod ( "null" ) ;
+                }
+            }
+            else
+            {
+                logMethod ( "null" ) ;
+            }
+
+            base.Before ( methodUnderTest ) ;
+        }
+
+        // ReSharper disable once InternalOrPrivateMemberNotDocumented
+        // ReSharper disable once MemberCanBeMadeStatic.Local
+        // ReSharper disable twice IdentifierTypo
+        private void LogMethod ( string message , string callerfilepath , string callermembername )
+        {
+            Logger?.Debug ( message ) ;
+            Debug.WriteLine ( message ) ;
+        }
+    }
 }
--- WpfApp1Tests3/WpfTests.cs
+++ WpfApp1Tests3/WpfTests.cs
@@ -8,115 +8,98 @@ using AppShared.Interfaces ;
 using Autofac ;
 using NLog ;
 using TestLib.Fixtures ;
-using WpfApp1Tests3.Attributes ;
 using WpfApp1Tests3.Fixtures ;
 using Xunit ;
 using Xunit.Abstractions ;
 
 namespace WpfApp1Tests3
 {
-	[ Collection ( "WpfApp" ) ]
-	public class WpfTests : WpfTestsBase
-	{
-		public WpfTests (
-			WpfApplicationFixture fixture
-		  , ContainerFixture      containerFixture
-		  , ObjectIDFixture       objectIdFixture
-		  , UtilsContainerFixture utilsContainerFixture
-		  , ITestOutputHelper     outputHelper
-		) : base (
-		          fixture
-		        , containerFixture
-		        , objectIdFixture
-		        , utilsContainerFixture
-		        , outputHelper
-		         )
-		{
-			Logger.Debug ( $"{nameof ( WpfTests )} constructor" ) ;
-		}
+    [ Collection ( "WpfApp" ) ]
+    public class WpfTests : WpfTestsBase
+    {
+        public WpfTests (
+            WpfApplicationFixture fixture
+          , ContainerFixture      containerFixture
+          , ObjectIDFixture       objectIdFixture
+          , UtilsContainerFixture utilsContainerFixture
+          , ITestOutputHelper     outputHelper
+        ) : base (
+                  fixture
+                , containerFixture
+                , objectIdFixture
+                , utilsContainerFixture
+                , outputHelper
+                 )
+        {
+            Logger.Debug ( $"{nameof ( WpfTests )} constructor" ) ;
+        }
 
-		private static readonly Logger Logger = LogManager.GetCurrentClassLogger ( ) ;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger ( ) ;
 
-		private void DoLog ( string test )
-		{
-			LB ( ).Level ( LogLevel.Trace ).Message ( test ).Write ( ) ;
-		}
+        private void DoLog ( string test )
+        {
+            LB ( ).Level ( LogLevel.Trace ).Message ( test ).Write ( ) ;
+        }
 
-		[ Fact ]
-		[ Trait ( "Experimental" , "true" ) ]
-		public void Test1 ( )
-		{
-			var menuItemList = containerScope.Resolve < IMenuItemList > ( ) ;
-			Assert.NotNull ( menuItemList ) ;
-			Assert.NotEmpty ( menuItemList ) ;
-			Fixture.MyApp.Resources[ "MyMenuItemList" ] = menuItemList ;
-			var found = Fixture.MyApp.FindResource ( "MyMenuItemList" ) ;
-			Assert.NotNull ( found ) ;
-			var x = string.Join ( ", " , menuItemList.First ( ).Children ) ;
-			Logger.Debug ( $"found {found}, {x}" ) ;
+        [ Fact ]
+        [ Trait ( "Experimental" , "true" ) ]
+        public void Test1 ( )
+        {
+            var menuItemList = containerScope.Resolve < IMenuItemList > ( ) ;
+            Assert.NotNull ( menuItemList ) ;
+            Assert.NotEmpty ( menuItemList ) ;
+            Fixture.MyApp.Resources[ "MyMenuItemList" ] = menuItemList ;
+            var found = Fixture.MyApp.FindResource ( "MyMenuItemList" ) ;
+            Assert.NotNull ( found ) ;
+            var x = string.Join ( ", " , menuItemList.First ( ).Children ) ;
+            Logger.Debug ( $"found {found}, {x}" ) ;
 
-			var uri = new Uri ( Fixture.BasePackUri , Resources.MenuResourcesPath ) ;
-			Logger.Debug ( $"{uri}" ) ;
+            var uri = new Uri ( Fixture.BasePackUri , Resources.MenuResourcesPath ) ;
+            Logger.Debug ( $"{uri}" ) ;
 
-			var stream = Application.GetResourceStream ( uri ) ;
-			Logger.Info ( stream.ContentType ) ;
-			var baml2006Reader = new Baml2006Reader ( stream.Stream ) ;
+            var stream = Application.GetResourceStream ( uri ) ;
+            Logger.Info ( stream.ContentType ) ;
+            var baml2006Reader = new Baml2006Reader ( stream.Stream ) ;
 
 
-			var o = XamlReader.Load ( baml2006Reader ) ;
-			Assert.NotNull ( o ) ;
-			///var o = Application.LoadComponent( uri );
-			var menuResources = o as ResourceDictionary ;
-			Assert.NotNull ( menuResources ) ;
-			//var stack = InstanceFactory.CreateContextStack < InfoContext >();
-			var stack = MyStack ;
-			var entry = myServices.InfoContextFactory ( nameof ( menuResources ) , menuResources ) ;
-			stack.Push ( entry ) ;
+            var o = XamlReader.Load ( baml2006Reader ) ;
+            Assert.NotNull ( o ) ;
+            ///var o = Application.LoadComponent( uri );
+            var menuResources = o as ResourceDictionary ;
+            Assert.NotNull ( menuResources ) ;
+            //var stack = InstanceFactory.CreateContextStack < InfoContext >();
+            var stack = MyStack ;
+            var entry = myServices.InfoContextFactory ( nameof ( menuResources ) , menuResources ) ;
+            stack.Push ( entry ) ;
 
-			foreach ( var q in menuResources.Keys )
-			{
-				var resource = menuResources[ q ] ;
-				stack.Push ( myServices.InfoContextFactory ( "key" , q ) ) ;
-				Logger.Debug ( $"{q}: {resource}" ) ;
-				var prefix = $"Resource[{q}]" ;
-				DumpHelper.DumpResource ( stack , resource , myServices.InfoContextFactory ) ;
-				stack.Pop ( ) ;
-			}
-		}
+            foreach ( var q in menuResources.Keys )
+            {
+                var resource = menuResources[ q ] ;
+                stack.Push ( myServices.InfoContextFactory ( "key" , q ) ) ;
+                Logger.Debug ( $"{q}: {resource}" ) ;
+                var prefix = $"Resource[{q}]" ;
+                DumpHelper.DumpResource ( stack , resource , myServices.InfoContextFactory ) ;
+                stack.Pop ( ) ;
+            }
+        }
+    }
 
-		[ Fact ]
-		[ Trait ( "MSBuild" , "Include" ) ]
-		[ PushContext ( "my pushed context" ) ]
-		public void Test2 ( )
-		{
-			using var c = C ( "using context" ) ;
-			DoLog ( "excellent message" ) ;
-		}
+#pragma warning disable 1591
+    public class MyServicesFixture
+#pragma warning restore 1591
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:System.Object" />
+        ///     class.
+        /// </summary>
+        public MyServicesFixture ( IMyServices myServices ) { MyServices = myServices ; }
 
-		[ Fact ]
-		[ Trait ( "MSBuild" , "Include" ) ]
-		public void Test3 ( )
-		{
-			using var c = C ( "testxx" ) ;
-			using var c2 = C ( "testxx2" ) ;
-			DoLog ( "test" ) ;
-		}
-	}
+        public IMyServices MyServices { get ; set ; }
+    }
 
-	public class MyServicesFixture
-	{
-		/// <summary>
-		///     Initializes a new instance of the <see cref="T:System.Object" />
-		///     class.
-		/// </summary>
-		public MyServicesFixture ( IMyServices myServices ) { MyServices = myServices ; }
+    public interface IMyServices
 
-		public IMyServices MyServices { get ; set ; }
-	}
-
-	public interface IMyServices
-
-	{
-		InfoContext.Factory InfoContextFactory { get ; }
-	}
+    {
+        InfoContext.Factory InfoContextFactory { get ; }
+    }
 }
