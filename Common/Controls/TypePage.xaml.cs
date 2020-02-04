f8f59481855f30cf2d2a9d42eb38c9770b735d61
Kay McCormick <kay@kaymccormick.com>
2020-02-03 19:42:26 -0800

    udpate

---
+++ Common/Controls/TypePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls ;
+
+namespace WpfTestApp
+{
+	/// <summary>
+	///     Interaction logic for TypePage.xaml
+	/// </summary>
+	public partial class TypePage : Page
+	{
+		public TypePage ( ) { InitializeComponent ( ) ; }
+	}
+}
